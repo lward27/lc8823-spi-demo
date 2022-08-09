78b2296e9ef9d30fd86d72198eb72c394fa14c56
Lucas WARD <lward@ipponusa.com>
2022-08-09 15:14:06 -0400

    moved some things around and added R when asleep

--- light_goggles.py
+++ light_goggles.py
@@ -1,4 +1,12 @@
 import asyncio
+import time
+
+# start = time.time()
+
+# print(23*2.3)
+
+# end = time.time()
+# print(end - start)
 
 class LightGoggles:
     def __init__(self, strip, sock):
@@ -12,23 +20,15 @@ class LightGoggles:
             lines = data.split(b'\n')
             # get the 4th line, or skip.
             if len(lines) < 4:
+                print("here lines < 4")
                 continue
+
             colors = lines[3]
-            #try:
-                #print(f"R:{colors[0]}, G:{colors[1]}, B:{colors[2]}")
-            #except:
             print(colors)
 
-            # print("parsed color: {}".format(colors))
-            # coerce colors as a list of ints
-            # colors2 = [int(c) for c in colors]
-            # print("parsed colors2: {}".format(colors2))
-            # if we have at least 3 colors values, display them in the strip, otherwise skip
             if len(colors) < 3:
+                print("here colors < 3")
                 continue
-        # y = b''.join([bytes([x[0]]), bytes([x[1]]), bytes([x[2]])]) 
-            # print("parsed lines: {}".format(lines))
-            # print(colored(colors[0], colors[1], colors[2], "O")) # show ANSI colors for debugging
 
             for i in range(self.strip.num_led):  # fill the strip with the same color
                 self.strip.set_pixel(i, 
@@ -37,9 +37,5 @@ class LightGoggles:
                     colors[2]//self.strip.global_brightness,
                     1)  # 1% brightness, but does not seem to make any difference
             self.strip.show()
-            await asyncio.sleep(0)
 
-    # async def get_new_variables(self):
-    #     while(True):
-    #         await asyncio.sleep(1)
-    #         print("Getting new variables now!")
+            await asyncio.sleep(0)
---
+++ main.py
@@ -0,0 +1,55 @@
+# main.py
+import asyncio
+from typing import Union
+
+import sys
+import led_driver
+import light_goggles
+import socket
+from constants import NUM_LED, UDP_IP, UDP_PORT, r, SPI_BUS, SPI_DEVICE, SPI_SPEED_HZ, BRIGHTNESS
+import goggle_light_show_templates
+from fastapi import FastAPI
+
+#Initialize Strip
+strip = led_driver.APA102(num_led=NUM_LED, 
+                            global_brightness=BRIGHTNESS, 
+                            SPI_BUS=SPI_BUS, 
+                            SPI_DEVICE=SPI_DEVICE,
+                            SPI_SPEED_HZ=SPI_SPEED_HZ)  # Initialize the strip
+
+#Initialize UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+#Initialize Goggles
+lg = light_goggles.LightGoggles(strip, sock)
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    loop = asyncio.get_running_loop()
+    #loop.create_task(lg.get_new_variables())
+    loop.create_task(lg.receive_vid_stream())
+    loop.create_task(goggle_light_show_templates.show_R(lg))
+    #await asyncio.gather(lg.get_new_variables())
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/goggles/brightness")
+async def read_brightness():
+    return {"brightness": lg.strip.global_brightness}
+
+@app.post("/goggles/brightness/{brightness}")
+async def set_brightness(brightness: int, q: Union[str, None] = None):
+    lg.strip.global_brightness = brightness
+    return {"brightness": brightness, "q": q}
+
+@app.post("/goggles/program/{program}")
+async def set_program(program: str, q: Union[str, None] = None):
+    return {"program": program, "q":q}
+
+
